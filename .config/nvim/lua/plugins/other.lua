f753a81529e0aa1d972545867a14fbedec571f3e
Ken Robertson <ken@invalidlogic.com>
2026-02-09 09:57:00 -0800

    Initial checkin.
    Initial checkin of Framework Desktop's config state.

---
+++ .config/nvim/lua/plugins/other.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = { preset = 'super-tab' },
+    }
+  },
+
+  {
+    "folke/snacks.nvim",
+    lazy = false,
+    opts = {
+      indent = {
+        enabled = true,
+      }
+    }
+  },
+}
